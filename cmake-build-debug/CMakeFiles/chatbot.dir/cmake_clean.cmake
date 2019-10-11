028f12a711bec96dfe95f2d6729e9a199ad2036c
Andrei27C <csatlos_andrei27@yahoo.com>
2019-10-11 15:19:35 +0300

    First

---
+++ cmake-build-debug/CMakeFiles/chatbot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatbot.dir/main.c.obj"
+  "chatbot.exe"
+  "chatbot.exe.manifest"
+  "chatbot.pdb"
+  "libchatbot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
